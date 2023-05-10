7e607985e1fcf0226ed9df89c2a2c76c61c89151
Anna Golubeva <agolubeva9483@gmail.com>
2023-05-10 14:31:35 +0300

    Csv commit

--- src/test/java/ru/netology/services/CountServiceTest.java
+++ src/test/java/ru/netology/services/CountServiceTest.java
@@ -2,16 +2,18 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
+    @CsvFileSource(files="src/test/resources/count.csv")
+    //@CsvSource({
+    //        "3,10000,3000,20000",
+    //        "2,100000,60000,150000"
 
-    })
+    //})
 
     public void testMonthVacation(int expected, int income, int expenses, int threshold) {
         CountService service = new CountService();
